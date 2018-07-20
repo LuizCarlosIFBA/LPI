eeb9004e0d76e8256c20e113f15a70bdf9bdce59
LuizCarlosIFBA <sacamento.tecnico@hotmail.com>
2018-07-20 11:39:21 -0300

    Q1 todos os ponteiros

--- Q1/main.c
+++ Q1/main.c
@@ -3,10 +3,10 @@
 #include "stdlib.h"
 #include "time.h"
 
-void CalcularMediaFinais(int n, int m, float nota[][n],float *pMediaFinal,int *pPeso){
+void CalcularMediaFinais(int *n, int *m, float  **nota,float *pMediaFinal,int *pPeso){
 	//Preenchendo o vetor peso 
 	srand((unsigned)time(NULL));
-	for(int i=0;i<n;i++){
+	for(int i=0;i<*n;i++){
    		pPeso[i] = rand()%11;
    		if(i % 10 == 0){
       			printf("\n");
@@ -20,8 +20,8 @@ void CalcularMediaFinais(int n, int m, float nota[][n],float *pMediaFinal,int *p
 
         float aux = 0;    
 	srand(time(NULL));
-	for(int j=0;j<m;j++){ 
- 		for(int i=0;i<n;i++){
+	for(int j=0;j<*m;j++){ 
+ 		for(int i=0;i<*n;i++){
    			aux = 0+ 10.0*((float)(rand())/RAND_MAX);
                         nota[j][i] = aux;
                         if(i % 10 == 0){
@@ -35,8 +35,8 @@ void CalcularMediaFinais(int n, int m, float nota[][n],float *pMediaFinal,int *p
        //Cálculo da média
        aux=0;
        
-       for(int j=0;j<m;j++){ 
- 		for(int i=0;i<n;i++){
+       for(int j=0;j<*m;j++){ 
+ 		for(int i=0;i<*n;i++){
    			aux+=nota[j][i]*pPeso[j]; 
 		}
     
@@ -45,18 +45,32 @@ void CalcularMediaFinais(int n, int m, float nota[][n],float *pMediaFinal,int *p
 }
 
 void main(){
-        int m = 2;
+        int m = 4;
  	int n = 4;	
-       
-        int peso[n];
-        float mediaFinal[m];
-	float nota[m][n];
+        
+	//ponteiro para m e n
+	int *pN = &n;
+	int *pM = &m;
 	
+	//vetores e matriz dinâmica 
+        int peso[*pN];
+        float mediaFinal[*pN];
+	float **nota;
+	
+	//aloco vetor principal
+	nota = malloc(4 *sizeof(sizeof(float *)));
+	
+	//aloco cada vetor linha
+	for(int i=0; i<4; i++){
+		nota[i] = (float *)malloc(4 * sizeof(float));
+	}
+
 	//Ponteiro peso, média e nota
-	int *pPeso = &peso[n]; 	
-	float *pMediaFinal = &mediaFinal[m];
-	    
-        CalcularMediaFinais(n,m,nota,pMediaFinal,pPeso);
+	int *pPeso = &peso[*pN]; 	
+	float *pMediaFinal = &mediaFinal[*pM];
+	  
+	void CalcularMediaFinais(int *n, int *m, float **nota,float *pMediaFinal,int *pPeso);  
+        CalcularMediaFinais(pN,pM,nota,pMediaFinal,pPeso);
         	
 }
 
