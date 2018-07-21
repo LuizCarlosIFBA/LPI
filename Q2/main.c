87ec6037a36dcbff3557214ebff958c43fb43218
LuizCarlosIFBA <sacamento.tecnico@hotmail.com>
2018-07-21 10:33:32 -0300

    Ponteiros questão 2

--- Q2/main.c
+++ Q2/main.c
@@ -3,44 +3,58 @@
 #include "stdlib.h"
 #include "time.h"
 #include <string.h>
-void desvioPadrao(float *MedFinal,int m, float Media, float DPadrao){
+void desvioPadrao(float *pMedFinal,int m, float *pMedia, float *pDPadrao){
 	float soma = 0;
-        float termo;
+        float termo = 0;
+	float *pSoma = &soma;
+	float *pTermo = &termo;
+
         int i;
 	for(i=0;i<m;i++){
-		soma += MedFinal[i];
+		*pSoma += pMedFinal[i];
 		
 	}
-	Media = soma/m;
+	*pMedia = *pSoma/m;
        
-        soma = 0;
-        termo = 0;
+        *pSoma = 0;
+        *pTermo = 0;
  	for(i=0;i<m;i++){
-		termo = MedFinal[i]-Media;
-		soma += pow(termo,2);
+		*pTermo = pMedFinal[i]-(*pMedia);
+		*pSoma += pow(*pTermo,2);
 	}
       
         //não é a coisa mais aconselhável a fazer, porém está ok 
 	//na variável diz somo, mas não foi somente essa operação 
         //realizada, mesmo assim o cálculo está certo. 
-	DPadrao = soma/(m-1);
+	*pDPadrao = *pSoma/(m-1);
         	
-        printf("A média aritmética é: %.1f \n",Media);
-	printf("O desvio padrão é: %.1f \n",sqrt(DPadrao));
+        printf("A média aritmética é: %.1f \n",*pMedia);
+	printf("O desvio padrão é: %.1f \n",sqrt(*pDPadrao));
 }
 
 void main(){
         int m = 2;         
-          
-        float Media;
+        int aux = 0; 
+	float Media = 0;
+	float DPadrao = 0;
+		
+	//ponteiro para variáveis 
+	int *pM = &m; 
+	int *pAux = &aux;
+	float *pMedia = &Media;
+	float *pDPadrao = &DPadrao;
+	// -----------------------
+	
 	float MedFinal[m];
-	float DPadrao;
+	
+	//ponteiro vetor
+	float *pMedFinal = MedFinal;
+	//------------------------
 
-	int i, aux; 
 	srand((unsigned)time(NULL));
-	for(i=0;i<m;i++){
-   		aux = 0+ 10.0*((float)(rand())/RAND_MAX);
-                MedFinal[i] = aux;
+	for(int i=0;i<*pM;i++){
+   		*pAux = 0+ 10.0*((float)(rand())/RAND_MAX);
+                pMedFinal[i] = *pAux;
                 //printf("%d º média final %.1f\n",i+1,MedFinal[i]);
    		if(i % 10 == 0){
       			printf("\n");
@@ -49,7 +63,7 @@ void main(){
    		
 	}
   
-	desvioPadrao(MedFinal,m,Media,DPadrao);
+	desvioPadrao(pMedFinal,m,pMedia,pDPadrao);
 }
 
 // gcc -o main main.c -lm
