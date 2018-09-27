03a546ad7ddc48ba422234ee44891c29fee4ad66
Luiz Carlos <sacramento.tecnico@hotmail.com>
2018-09-26 23:28:46 -0300

    Add files via upload

--- Q4/main.c
+++ Q4/main.c
@@ -8,22 +8,22 @@ struct nome{
 	char nome[50];
 };
 
-void desvioPadrao(float *pMedFinal,int *m, float *pMedia, float *pDPadrao){
+void desvioPadrao(float *pMedFinal,int m, float *pMedia, float *pDPadrao){
 	float soma = 0;
         float termo = 0;
 	float *pSoma = &soma;
 	float *pTermo = &termo;
 
         int i;
-	for(i=0;i<*m;i++){
+	for(i=0;i<m;i++){
 		*pSoma += pMedFinal[i];
 		
 	}
-	*pMedia = *pSoma/(*m);
+	*pMedia = *pSoma/m;
        
         *pSoma = 0;
         *pTermo = 0;
- 	for(i=0;i<*m;i++){
+ 	for(i=0;i<m;i++){
 		*pTermo = pMedFinal[i]-(*pMedia);
 		*pSoma += pow(*pTermo,2);
 	}
@@ -31,22 +31,22 @@ void desvioPadrao(float *pMedFinal,int *m, float *pMedia, float *pDPadrao){
         //não é a coisa mais aconselhável a fazer, porém está ok 
 	//na variável diz somo, mas não foi somente essa operação 
         //realizada, mesmo assim o cálculo está certo. 
-	*pDPadrao = *pSoma/(*m-1);
+	*pDPadrao = *pSoma/(m-1);
         	
         printf("A média aritmética é: %.1f \n",*pMedia);
 	printf("O desvio padrão é: %.1f \n",sqrt(*pDPadrao));
 }			     	
 
-void AlunoDestaque(int *n, int *m, float **nota,float *mediaFinal,int *peso){
+void AlunoDestaque(int n, int m, float **nota,float *mediaFinal,int *peso){
 	 typedef struct {
         	char nome[100];
 	 } Aluno;
  	 
 	 //'m' recebeu os valores de TAM
-         Aluno alunos[*m];
+         Aluno alunos[m];
          
         
-         for(int i= 0;i<*m;i++){
+         for(int i= 0;i<m;i++){
 		printf("\nEscreva o nome do aluno(%i): ",i+1);
                   setbuf(stdin, NULL);
                  scanf("%[^\n]i",alunos[i].nome);
@@ -57,7 +57,7 @@ void AlunoDestaque(int *n, int *m, float **nota,float *mediaFinal,int *peso){
 	int i,j; 
 	i = 0;
 	do{
-		for(j=0;j<*n;j++){
+		for(j=0;j<n;j++){
 			printf("\nEscreva %d º  nota: ",j+1);
    			scanf("%f",&nota[j][i]);
                         if(i % 10 == 0){
@@ -65,12 +65,12 @@ void AlunoDestaque(int *n, int *m, float **nota,float *mediaFinal,int *peso){
         		} 
 	        }
 	i++;
-	}while(i<(*m));	
+	}while(i<m);	
 
 	//Preenchendo o vetor peso 
 
 	srand((unsigned)time(NULL));
-	for(i=0;i<*n;i++){
+	for(i=0;i<n;i++){
    		peso[i] = rand()%11;
    		if(i % 10 == 0){
       			//printf("\n");
@@ -83,8 +83,8 @@ void AlunoDestaque(int *n, int *m, float **nota,float *mediaFinal,int *peso){
        //Cálculo da média
        float aux=0;
        
-       for(j=0;j<*n;j++){ 
- 		for(i=0;i<*n;i++){
+       for(j=0;j<n;j++){ 
+ 		for(i=0;i<n;i++){
    			aux+=nota[j][i]; 
 		}
     
@@ -94,8 +94,8 @@ void AlunoDestaque(int *n, int *m, float **nota,float *mediaFinal,int *peso){
      //-----------------------------------------------------------------------//
      //lógica para aluno destaque
      
-     for(int i=0;i<*m;i++){ 
- 	 for(int j=0;j<*m;j++){
+     for(int i=0;i<m;i++){ 
+ 	 for(int j=0;j<m;j++){
    	 	aux = 0;
 		if(mediaFinal[i]<mediaFinal[j]){
 			aux = mediaFinal[j];
@@ -107,23 +107,23 @@ void AlunoDestaque(int *n, int *m, float **nota,float *mediaFinal,int *peso){
 	
    
      int destaque = 0;
-     for(int j=0;j<*n;j++){
-         if(mediaFinal[*m]>=7){
-	     destaque = nota[*m][j]<=5?destaque+0:destaque+1;	
-	 }else printf("Não houve aluno destaque");
+     for(int j=0;j<n;j++){
+         if(mediaFinal[m]>=7){
+	     destaque = nota[m][j]<=5?destaque+0:destaque+1;	
+	 }
      }
     
      if(destaque>0){
 	printf("Os alunos destaque são:");     
-	printf("%c",alunos[*m].nome[100]);
-     }
+	printf("%c",alunos[m].nome[100]);
+     }else printf("Não houve aluno destaque");
 
      
-     for(int j=*m-1;j==0;j--){
+     for(int j=m-1;j==0;j--){
 	destaque = 0;
-	if(mediaFinal[j]>=7 && mediaFinal[j]==mediaFinal[*m]){
-        	if(mediaFinal[*m]>=7){
-	     		destaque = nota[*m][j]<=5?destaque+0:destaque+1; 
+	if(mediaFinal[j]>=7 && mediaFinal[j]==mediaFinal[m]){
+        	if(mediaFinal[m]>=7){
+	     		destaque = nota[m][j]<=5?destaque+0:destaque+1; 
 		}  	
 	}
 	
@@ -165,29 +165,24 @@ void main(){
 		scanf("%d",&n);
 	}
         
-	//ponteiro TAM
-	int *pTAM = &m;	
-	int *pN = &n;	
-	// -----------------------
-	
      	//vetores e matriz dinâmica 
-        int peso[*pN];
-        float mediaFinal[*pTAM];
+        int peso[n];
+        float mediaFinal[TAM];
 	float **nota;
 
 	//aloco vetor principal
-	nota = malloc((*pN) *sizeof(sizeof(float *)));
+	nota = malloc((n) *sizeof(sizeof(float *)));
 	
 	//aloco cada vetor linha
-	for(int i=0; i<*pN; i++){
-		nota[i] = (float *)malloc((*pTAM)* sizeof(float));
+	for(int i=0; i<n; i++){
+		nota[i] = (float *)malloc((TAM)* sizeof(float));
 	}
 	
 	//Ponteiro peso, média e nota
-	int *pPeso = &peso[*pN]; 	
-	float *pMediaFinal = &mediaFinal[*pTAM];
+	int *pPeso = &peso[n]; 	
+	float *pMediaFinal = &mediaFinal[TAM];
 
 
-        AlunoDestaque(pN,pTAM,nota,pMediaFinal,pPeso);
+        AlunoDestaque(n,TAM,nota,pMediaFinal,pPeso);
 }
 // gcc -o main main.c -lm
